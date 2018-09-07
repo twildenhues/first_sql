250afad32d26e50287153781996f1c5ac492ceb4
Tim Wildenhues <tim.wildenhues@tobit.software>
2018-09-07 16:02:57 +0200

    Saving

--- Database1/view/viEmployee.sql
+++ Database1/view/viEmployee.sql
@@ -1,5 +1,5 @@
 ﻿CREATE VIEW [dbo].[viEmployee]
-	AS SELECT e.Id, e.FirstName, e.LastName, e.Birthday, e.DepartementId, e.CreatedTime, a.Country, a.City, a.ZIP, a.Street FROM [Employee] e 
+	AS SELECT e.Id, e.FirstName, e.LastName, e.Birthday, e.DepartementId, e.CreatedTime, a.Country, a.City, a.Zip, a.Street FROM [Employee] e 
 		LEFT OUTER JOIN Address2Employee a2e
 			ON a2e.Id = e.Id 
 		LEFT OUTER JOIN Address a
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -32,6 +32,35 @@ namespace SqlTest_CSharp
 							reader[0], reader[1], reader[2], reader[3], reader[4], reader[5], reader[6]));
 					}
 				}
+				SqlCommand viewDepartement = new SqlCommand("SELECT * FROM viDepartement", conn);
+
+				Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------");
+
+				using (SqlDataReader reader = viewDepartement.ExecuteReader())
+				{
+					Console.WriteLine("Id\t\tCompanyId\t\tManagerId\t\tDepartementName\t\tCreatedTime\t");
+					while (reader.Read())
+					{
+						Console.WriteLine(String.Format("{0} \t | {1} \t\t | {2} \t | {3} \t | {4} \t |",
+							reader[0], reader[1], reader[2], reader[3], reader[4]));
+					}
+				}
+				SqlCommand viewEmployee = new SqlCommand("SELECT * FROM viEmployee", conn);
+
+				Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------");
+
+				using (SqlDataReader reader = viewEmployee.ExecuteReader())
+				{
+					Console.WriteLine("Id\t\tFirst Name\t\tLast Name\t\tBirthday\t\tDepartementId\t\tCreatedTime\t\tCountry\t\tCity\t\tZip\t\tStreet\t");
+					while (reader.Read())
+					{
+						Console.WriteLine(String.Format("{0} \t | {1} \t\t | {2} \t | {3} \t | {4} \t | {5} \t | {6} \t | {7} \t | {8} \t | {9} \t |",
+							reader[0], reader[1], reader[2], reader[3], reader[4], reader[5], reader[6], reader[7], reader[8], reader[9]));
+					}
+				}
+
+				Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------");
+
 				Console.WriteLine("Data displayed! Now press enter to move to the next section!");
 				Console.ReadLine();
 				Console.Clear();
