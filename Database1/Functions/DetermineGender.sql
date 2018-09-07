c9bdf6a6e3d8051906e958fb91e53ce8c5cc7771
Tim Wildenhues <tim.wildenhues@tobit.software>
2018-09-07 09:24:02 +0200

    Function to Identify Gender

---
+++ Database1/Functions/DetermineGender.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION dbo.determine_gender(@genderAsInt int)    
+	RETURNS nvarchar(64)    
+	AS 
+    BEGIN   
+		RETURN(	
+				SELECT CASE @genderAsInt	
+					WHEN 1 THEN 'Male'
+					WHEN 2 THEN 'Female'
+					WHEN 3 THEN 'something else' 
+					ELSE		'no gender given'
+				END
+		)
+END
