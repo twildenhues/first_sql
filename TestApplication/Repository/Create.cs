9eb53ac5687f4c2cd9ab0aefcdd48e2b75ca8601
Tim Wildenhues <tim.wildenhues@tobit.software>
2018-09-10 14:17:09 +0200

    Saving

--- TestApplication/Repository/Create.cs
+++ TestApplication/Repository/Create.cs
@@ -46,23 +46,64 @@ class Create
 			{
 				case '1':
 					Console.WriteLine(" ");
-					Console.WriteLine("Please enter the name of the Company:");
-						insertCommand.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar).Value = Console.ReadLine();
+						Console.WriteLine("Please enter now the Id, if you want to change the name of an existing Company. Else just press enter to generate a new Company");
+							string tempCompany = Console.ReadLine();
+							int CompanyId;
+							Int32.TryParse(tempCompany, out CompanyId);
+							insertCommand.Parameters.AddWithValue("@Id", (CompanyId == 0) ? -1 : CompanyId);
 					Console.WriteLine(" ");
-					Console.WriteLine("Please enter now the Id:");
-					insertCommand.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = Console.ReadLine();
+						Console.WriteLine("Please enter the name of the Company:");
+							insertCommand.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar).Value = Console.ReadLine();
 					break;
 				case '2':
-					insertCommand.Parameters.Add(new SqlParameter("0", 10));
-					insertCommand.Parameters.Add(new SqlParameter("1", "Test Column"));
-					insertCommand.Parameters.Add(new SqlParameter("2", DateTime.Now));
-					insertCommand.Parameters.Add(new SqlParameter("3", false)); ;
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter now the Id, if you want to change the name of an existing Departement. Else just press enter to generate a new Departement:");
+							string tempDepartement = Console.ReadLine();
+							int DepartementId;
+							Int32.TryParse(tempDepartement, out DepartementId);
+							Console.WriteLine(DepartementId);
+							insertCommand.Parameters.AddWithValue("@Id", (DepartementId == 0) ? -1 : DepartementId);
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter the name of the Departement:");
+							insertCommand.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar).Value = Console.ReadLine();
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter the Id of the Company this Departement belongs to. If you want to do it later, press enter:");
+							string temp = Console.ReadLine();
+							int id;
+							Int32.TryParse(temp, out id);
+							insertCommand.Parameters.AddWithValue("@CompanyId", (id == 0) ? -1 : id);
 					break;
 				case '3':
-					insertCommand.Parameters.Add(new SqlParameter("0", 10));
-					insertCommand.Parameters.Add(new SqlParameter("1", "Test Column"));
-					insertCommand.Parameters.Add(new SqlParameter("2", DateTime.Now));
-					insertCommand.Parameters.Add(new SqlParameter("3", false)); ;
+					/*@FirstName 
+					@LastName
+					@Birthday
+					@DepartementId 
+					@Gender
+					@Id*/
+					Console.WriteLine(" ");
+						Console.WriteLine("Enter the Id of the an existing person if you want to change something, and only press enter to generate a new person:");
+							string tempEmployee = Console.ReadLine();
+							int EmployeeId;
+							Int32.TryParse(tempEmployee, out EmployeeId);
+							insertCommand.Parameters.AddWithValue("@Id", (EmployeeId == 0) ? -1 : EmployeeId);
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter the first name of the Employee:");
+							insertCommand.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar).Value = Console.ReadLine();
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter the last name of the Employee:");
+							insertCommand.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar).Value = Console.ReadLine();
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter the birthday of the Employee and use this syntax YYYY-MM-DD:");
+							insertCommand.Parameters.Add("@Name", System.Data.SqlDbType.Date).Value = Console.ReadLine();
+					Console.WriteLine(" ");
+						Console.WriteLine("Please enter the gender of the Employee ..");
+					Console.WriteLine("enter '1' for male");
+					Console.WriteLine("enter '2' for female");
+					Console.WriteLine("enter '3' for something else");
+					string tempGender = Console.ReadLine();
+					int Gender;
+					Int32.TryParse(tempGender, out Gender);
+					insertCommand.Parameters.AddWithValue("@Id", (Gender == 0) ? -1 : Gender);
 					break;
 				default:
 					insertCommand.Parameters.Add(new SqlParameter("0", 10));
@@ -73,7 +114,7 @@ class Create
 			}
 
 			Console.WriteLine("Commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery());
-			Console.WriteLine("Done! Press enter to move to the next step");
+			Console.WriteLine("Done! Press enter to clear the Console");
 			Console.ReadLine();
 			Console.Clear();
 		};
